3741a36ab339aae566d616132a2e8e9d46ef3362
NewesGordon <1481239341@qq.com>
2019-06-26 09:10:20 +0800

    can fix several file once

--- main.cpp
+++ main.cpp
@@ -1,41 +1,59 @@
-#include <QCoreApplication>
+﻿#include <QCoreApplication>
 #include <QFile>
 #include <QTextStream>
 #include <QDebug>
 
-int main(int argc, char *argv[])//.exe old.txt new.txt
+int main(int argc, char *argv[])//.exe old0.txt old1.txt ... new.txt
 {
     QCoreApplication a(argc, argv);
 
-    QFile oldFile(argv[1]);//待修改文件
-    QFile newFile(argv[2]);//修改后保存的新文件
+    if(argc < 3){
+        qDebug() << "too less paras.";
+        return a.exec();
+    }
+    QList<QFile*> oldFileList;//待读取文件列表
+    for(int i=1; i<argc-1; i++){
+        QFile *oldFile = new QFile(argv[i]);//待修改文件
+        oldFileList.append(oldFile);
+    }
+    QFile newFile(argv[argc-1]);//修改后保存的新文件
 
     newFile.open(QIODevice::Text | QIODevice::WriteOnly | QIODevice::Append);//打开新文件
     QTextStream out(&newFile);//定义写文件流
 
-    oldFile.open(QIODevice::ReadOnly | QIODevice::Text);//打开旧文件
-    QString line;//缓存每一行读取
-    int count = 0;
-    //循环读取新的行
-    while(!oldFile.atEnd()){
-        line = oldFile.readLine();//读取新一行
-        if(line != "\n"){//忽略空行
-            line = line.left(line.indexOf("//")).simplified();
-            if(line != ""){//删除注释后，忽略空行
-                if(line.contains("\n"))
-                    out << line;//写入新行
-                else
-                    out << line << endl;//写入新行
-                qDebug() << "第" << ++count << "行";
+    int count = 0;//行计数
+
+    //循环读取文件
+    for(int i=0; i<oldFileList.length(); i++){
+        oldFileList[i]->open(QIODevice::ReadOnly | QIODevice::Text);//打开旧文件
+        QString line;//缓存每一行读取
+        //循环读取新的行
+        while(!oldFileList[i]->atEnd()){
+            line = oldFileList[i]->readLine();//读取新一行
+            if(line != "\n"){//忽略空行
+                line = line.left(line.indexOf("//")).simplified();
+                if(line != ""){//删除注释后，忽略空行
+                    if(line.contains("\n"))
+                        out << line;//写入新行
+                    else
+                        out << line << endl;//写入新行
+                    qDebug() << "Line No." << ++count;
+                }
             }
         }
+        oldFileList[i]->close();
     }
-
-    oldFile.close();
+    //关闭文件
     newFile.close();
 
+    //释放内存
+    for(int i=0; i<oldFileList.length(); i++){
+        delete oldFileList.at(i);
+    }
+    oldFileList.clear();
+
     qDebug() << "---------Done!----------";
-    qDebug() << "共" << count << "行代码.";
+    qDebug() << "total:" << count << "lines.";
 
     return a.exec();
 }
